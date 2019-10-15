eca4d6ac0c6aacbbc51fa273445927f96f2b73f6
ZhangYu <1257068932@qq.com>
2019-10-15 19:45:32 +0800

    zy

--- BF_zy/routers/film.js
+++ BF_zy/routers/film.js
@@ -16,5 +16,15 @@ router.post(films, controller.addFilm)
 // 删除影片
 router.delete(`${films}/:id(\\d+)`, controller.deleteFilm)
 
+// 查询单个/回显影片
+router.get(`${films}/:id(\\d+)`, controller.inquireFilm)
+
+// 修改影片
+router.put(`${films}/:id(\\d+)`, controller.upFilm)
+
+// 查询多个影片
+router.get(films, controller.inquireFilms)
+
+
 // 暴露路由
 module.exports = router
