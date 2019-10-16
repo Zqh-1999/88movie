03951964685f5f9db892fa0c266e820cb732e2f6
chenk0817 <2185824248@qq.com>
2019-10-16 20:09:06 +0800

    day02

--- playr/app.js
+++ playr/app.js
@@ -1,8 +1,16 @@
-
 const express = require('express');
 
 // 创建服务器
 const app = express();
+// 1.设置静态资源目录
+app.use(express.static('./public'));
+
+app.use('api/v1', require('./router/iRouter'))
+// 首页渲染
+app.get('/', require('./router/iRouter'))
+
+
+
 //#region 3.配置ejs模板引擎
 // 3.1 下载ejs包
 // 3.2 设置模板引擎的后缀是什么
@@ -13,16 +21,6 @@ app.set('views', __dirname + '/views');
 //#endregion
 
 
-const chekai=require('./views/chenkai.ejs');
-
-
-app.get('/',(req,res)=>{
-    res.render('chenkai')
-})
-
-
-// 1.设置静态资源目录
-app.use(express.static('./public'));
 // 监听端口 并启动服务
 app.listen(8000, () => {
     console.log('server is running at http://localhost:8000');
