11dea8f4326b8f15f6fdbafb425bc0c8174b2426
chenk0817 <2185824248@qq.com>
2019-10-16 09:35:37 +0800

    day02

---
+++ playr/app.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+
+// 创建服务器
+const app = express();
+//#region 3.配置ejs模板引擎
+// 3.1 下载ejs包
+// 3.2 设置模板引擎的后缀是什么
+var ejs = require('ejs')
+app.set('view engine', 'ejs');
+// 3.3 设置模板引擎的模板在哪个目录
+app.set('views', __dirname + '/views');
+//#endregion
+
+app.get('/',(req,res)=>{
+    res.render('chenkai')
+})
+// 1.设置静态资源目录
+app.use(express.static('./public'));
+// 监听端口 并启动服务
+app.listen(8000, () => {
+    console.log('server is running at http://localhost');
+})
