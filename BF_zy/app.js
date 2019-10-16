a3ecbad0470ccb6fe4da30bc066e9a72ef45c1ec
ZhangYu <1257068932@qq.com>
2019-10-16 10:45:02 +0800

    ok data

--- BF_zy/app.js
+++ BF_zy/app.js
@@ -16,14 +16,27 @@ const app = express()
 // 配置bdParser
 app.use(bdParser.urlencoded({extends:true}))
 
+// 跨域
+// var cors = require('cors')
+// app.use(cors())
+
+// var corsOptions = {
+//     origin: 'http://127.0.0.1',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
+// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//     res.json({ msg: 'This is CORS-enabled for only example.com.' })
+// })
+
 // 配置影片路由
 app.use(v1, require('./routers/films'))
-// // 配置管理员路由
-// app.use(v1, require('./routers/supUsers'))
-// // 配置用户路由
-// app.use(v1, require('./routers/user'))
-// // 配置历史纪录路由
-// app.use(v1, require('./routers/history'))
+// 配置管理员路由
+app.use(v1, require('./routers/supUsers'))
+// 配置用户路由
+app.use(v1, require('./routers/user'))
+// 配置历史纪录路由
+app.use(v1, require('./routers/history'))
 
 // 启动服务
 app.listen(serve.prot, () => {
