fdc8686ddc50b6ce77c471bd53d51f9f8aa3a039
dujianing <2636573612@qq.com>
2019-10-16 17:52:33 +0800

    day02

--- dujianing/public/js/intro.js
+++ dujianing/public/js/intro.js
@@ -4,9 +4,31 @@ $('.cource_tab li').click(function () {
 })
 
 
+var nav = document.querySelector(".intro_explain");
+var con = document.querySelector(".conceal");
+con.onclick = function () {
+  if (con.innerHTML = "展开") {
+  con.innerHTML = "隐藏";
+  }
+}
 
-
-
+// #region 用户登录
+$('.btn-dow').on('click', function () {
+  let id = $(this).data('id');
+  $.ajax({
+    type: 'get',
+    url: '/admin/login',
+    data: {id: id},
+    success: function (data) {
+      if (data.code == '1201') {
+        alert(data.message);
+      } else if (data.code == '1202') {
+        location.href = '/admin/index';
+      }
+    }
+  })
+})
+// #endregion
 
 
 
