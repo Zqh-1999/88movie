020f49443c96b56a1696d40f7e276e0e5e29561e
dujianing <2636573612@qq.com>
2019-10-17 17:58:58 +0800

    day03

--- dujianing/public/js/note.js
+++ dujianing/public/js/note.js
@@ -7,3 +7,15 @@ $("#close").click(function () {
     $(".hide-center").fadeOut("slow")
     $(".overCurtain").fadeOut("slow")
 })
+
+
+
+$(".loginPassword").on("click", ".fa-eye-open", function () {
+    $(this).removeClass("fa-eye-open").addClass("fa-eye");
+    $(this).prev().attr("type", "text");
+    });
+     
+    $(".loginPassword").on("click", ".fa-eye", function () {
+    $(this).removeClass("fa-eye").addClass("fa-eye-open");
+    $(this).prev().attr("type", "loginPassword");
+    });
