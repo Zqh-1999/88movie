c0302ea75e10b1eabc3fbd4ecf07ff1b0fd2764c
zyz15995056571 <1932671590@qq.com>
2019-10-18 14:24:32 +0800

    zhouyunzhuo

--- zhouyunzhuo/routes/logon.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-// 2.使用Router来创建路由
-const router = express.Router();
-const controllerLogon=require('../controller/logon');
-router.get('/logon', controllerLogon.logon);
-module.exports = router;
