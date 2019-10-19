8051fb7c4c030727a5d68072bad8cc8799231213
ZhangYu <1257068932@qq.com>
2019-10-19 10:36:26 +0800

    hui yuan

--- BF_zy/routers/recharge.js
+++ BF_zy/routers/recharge.js
@@ -13,16 +13,6 @@ const recharges = '/recharge'
 // 添加会员
 router.post(recharges, controller.addrecharge)
 
-
-// // 查询单个/回显会员
-// router.get(`${recharges}/:id(\\d+)`, controller.inquirerecharge)
-
-// // 修改会员
-// router.put(`${recharges}/:id(\\d+)`, controller.uprecharge)
-
-// // 查询多个会员
-// router.get(recharges, controller.inquirerecharges)
-
 // // 查询所有会员
 router.get(`${recharges}/all`, controller.inquirerechargeAll)
 
